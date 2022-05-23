cc67d275d655d3687ee047658228b1468480483b
Ruslan Gula <ruslan3gula@gmail.com>
2022-05-24 00:53:15 +0300

    component BaseCurrency  added

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import "./App.css";
 
 import { Converter } from "./components/Converter";
 import { Currencies } from "./components/Currencies";
+import { BaseCurrency } from "./components/BaseCurrency";
 
 function App() {
   return (
     <div className="App">
+      <BaseCurrency />
       <Converter />
       <Currencies />
     </div>
