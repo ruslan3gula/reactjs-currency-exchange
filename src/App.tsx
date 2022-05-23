15845c2240ada61c510aeead36e6800ea4a5dfda
Ruslan Gula <ruslan3gula@gmail.com>
2022-05-24 00:03:35 +0300

    project converted to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./App.css";
 
 import { Converter } from "./components/Converter";
--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./Converter.css";
 
 export const Converter = () => {
